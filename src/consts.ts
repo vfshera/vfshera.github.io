c693b43c4842c58402cc22661d208a374a6e82ca
Franklin Shera <fshera96@gmail.com>
2023-03-13 15:10:33 +0300

    consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Franklin Shera";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const DOMAIN = "http://vfshera.github.io";
